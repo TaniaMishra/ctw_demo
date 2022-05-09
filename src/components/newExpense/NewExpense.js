da0a20fac245efcd64424cbef8032874478aa805
TaniaMishra <tania.r.mishra@gmail.com>
2022-05-09 11:16:08 -0500

    progress check 8-1

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -3,9 +3,18 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm.js'
 
 function NewExpense(){
+    function saveExpenseDataHandler(enteredExpenseData) { 
+        //The parameter will be the data collected at form submission
+        const expenseData = {
+            ...enteredExpenseData, 		//Break apart the fields of the object
+            id: Math.random().toString()	//Every element needs a unique ID
+        };
+        console.log(expenseData);		//Display to the screen to make sure it works.
+    }
+
     return(
         <div className='new-expense'>
-            <ExpenseForm></ExpenseForm>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
         </div>
     );
 }
