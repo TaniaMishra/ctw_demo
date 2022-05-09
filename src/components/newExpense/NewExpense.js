d19b32d4c64f9257f677c579d422a64226b525e4
TaniaMishra <tania.r.mishra@gmail.com>
2022-05-09 11:23:40 -0500

    progress check 8-2

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm.js'
 
-function NewExpense(){
+function NewExpense(props){
     function saveExpenseDataHandler(enteredExpenseData) { 
         //The parameter will be the data collected at form submission
         const expenseData = {
             ...enteredExpenseData, 		//Break apart the fields of the object
             id: Math.random().toString()	//Every element needs a unique ID
         };
-        console.log(expenseData);		//Display to the screen to make sure it works.
+        props.onAddExpense(expenseData);
     }
 
     return(
